6d47828a99076277e1e05db80b13f67ce79cdbf9
unknown <SerDImIgor@gmail.com>
2022-05-16 21:30:07 +0300

    first project Django

---
+++ first_project/mysite/base/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('login/',views.login_page,name="login"),
+    path('logout/',views.logout_page,name="logout"),
+    path('register/',views.register_page,name="register"),
+    path('profile/<int:pk>',views.user_profile,name="user_profile"),
+    
+    path('',views.home,name='home'),
+    path('room/<str:pk>',views.room,name='room'),
+    path('create-room',views.create_room,name='create_room'),
+    path('update-room/<int:pk>',views.update_room,name='update_room'),
+    path('delete-room/<int:pk>',views.delete_room,name='delete_room'),
+    path('delete-message/<int:pk>',views.delete_message,name='delete_message'),
+    path('update-user/',views.update_user,name='update_user'),
+    path('topics/',views.topics_page,name='topics'),
+    path('activity/',views.activity_page,name='activity'),
+    
+
+
+
+]
